074b3e49d7a9dca7ad462a7b6442ea97c7034af0
Juan <jppuerta@noel.com.co>
2023-02-05 14:35:45 -0500

    sttyll

--- src/components/AllProposals/AllProposalsView.js
+++ src/components/AllProposals/AllProposalsView.js
@@ -10,6 +10,12 @@ export const AllProposalsView =()=>{
     const {proposals} = useContext(ProposalsContext)
     const [proposalDetail, setProposalDetail] = useState({});
     const [showDetail, setShowDetail] = useState (false)
+
+    const handleAproved =(proposal)=> {
+    proposal.stateAproved=25    
+    setShowDetail(!showDetail)    
+    }
+
     const navigate = useNavigate()
 
     const handleAddProposal =()=>{
@@ -26,7 +32,7 @@ export const AllProposalsView =()=>{
         return(  
             <>  
             <ButtonNav handleNav={handleAddProposal}goTo='add a new proposal'/>  
-            <div className='d-flex justify-content-around'>
+            <div className='d-flex justify-content-between'>
                 <div className='d-flex flex-column '>
                 {proposals.map(proposal=>
                 <ProposalCardView
@@ -35,8 +41,9 @@ export const AllProposalsView =()=>{
                     handleDetail = {handleDetail}
                 />)}                              
                 </div>    
-                <div>
-                    <ProposalDetailView proposal = {proposalDetail}/>
+                <div className='border border-success'>
+                    <ProposalDetailView proposal = {proposalDetail}
+                    handleAproved={handleAproved}/>
                 </div>           
                   
             </div>
--- src/components/ProposalDetail/ProposalDetailView.js
+++ src/components/ProposalDetail/ProposalDetailView.js
@@ -2,22 +2,30 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import { ProgressBar } from 'react-bootstrap'
 
-export const ProposalDetailView =({proposal})=> {
+export const ProposalDetailView =({proposal, handleAproved})=> {
+
     return(
         <div>
-            <h1>{proposal.id} - {proposal.nameIdea}</h1>
-            <p>{proposal.description}</p>
-            <h2>{proposal.cost}</h2>
-            <h3>{proposal.area}-{proposal.oven}</h3>
-            <h3>jefe {proposal.boss}</h3>
-            <ProgressBar className='p-10'>
-            <ProgressBar striped variant="danger" now={proposal.stateCreate} label='propuesta'/>
-            <ProgressBar variant="warning" now={proposal.stateAproved} label='Ok jefe'/>
-            <ProgressBar variant="info" now={proposal.stateComment} label='comments'/>
-            <ProgressBar striped variant="success" now={proposal.stateDecline} label='Ok director'/>
-            </ProgressBar>
-            <div className='d-flex justify-content-around'>
-                <Button variant="outline-primary">Aprobar</Button>
+            <div>
+
+                <h2>{proposal.id} - {proposal.nameIdea}</h2>
+                <p>{proposal.description}</p>
+                <h2>{proposal.cost}</h2>
+                <h3>{proposal.area}-{proposal.oven}</h3>
+                <h3>jefe {proposal.boss}</h3>
+            </div>
+            <div>
+                <h3>Progress</h3>
+                <ProgressBar>
+                <ProgressBar striped variant="danger" now={proposal.stateCreate} label='propuesta'/>
+                <ProgressBar variant="warning" now={proposal.stateAproved} label='Ok jefe'/>
+                <ProgressBar variant="info" now={proposal.stateComment} label='comments'/>
+                <ProgressBar striped variant="success" now={proposal.stateDecline} label='Ok director'/>
+                </ProgressBar>
+            </div>
+            <h3>actions</h3>
+            <div className=' d-flex m-50 justify-content-around'>
+                <Button variant="outline-primary" onClick={()=>handleAproved(proposal)}>Aprobar</Button>
                 <Button variant="outline-warning">Comentar</Button>
                 <Button variant="outline-danger">Rechazar</Button>
             </div>
